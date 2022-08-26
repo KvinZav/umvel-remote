d8cbcda20ee37a46757b75ace0e71cd6da6a1cf5
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-08-26 13:18:47 -0500

    feat: create scroll interaction component

---
+++ src/components/modules/ScrollInteraction1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ScrollInteraction1 = () => {
+  return(
+    <>
+      <section>
+        <div></div>
+      </section>
+    </>
+  )
+}
+
+const TextBlock = ({children}) => {
+  <h1>Making</h1>
+}
+
+export default ScrollInteraction1;
