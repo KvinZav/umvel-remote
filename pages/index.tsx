b524d45dcffe4ba5665fbfd366f38f861d85dea3
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-08-30 14:54:37 -0500

    feat: scrolling interaction right side

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,13 @@ import ScrollInteraction1 from '@modules/ScrollInteraction1';
 import { useAppState } from '@hooks/customHooks';
 import { useEffect } from 'react';
 
-// import dynamic from 'next/dynamic';
-// const Animator = dynamic(
-//   import("react-scroll-motion").then((it) => it.Animator),
-//   { ssr: false }
-// );
-import { Fade, MoveIn } from 'react-scroll-motion';
-
 export default function Home() {
   const isBrowser = typeof window !== "undefined"
 
   const { data, error } = useSWR(environment.HOME_URL, get, {
     revalidateOnFocus: false,
   });
-
-  console.log(data);
   
-
   const { handleScroll, scrollOffset } = useAppState()
 
   const onScroll = () => handleScroll(isBrowser ? window.pageYOffset : 0)
@@ -34,15 +24,10 @@ export default function Home() {
   
   return data && (
     <div>
-      {/* <Highlights
-        cases={data?.data?.attributes.body.find(i => i.__component === 'highlights.highlights').cases}
-      /> */}
-      {/* <div className="h-screen"/> */}
       <ScrollInteraction1
+        steps={data?.data?.attributes.body.find(i => i.__component === "scroll-interaction.scroll-interaction").step}
         offsetY={scrollOffset}
       />
-      {/* <div className="h-screen"/>
-      <div className="h-screen"/> */}
     </div>
   )
 }
--- src/components/modules/ScrollInteraction1.tsx
+++ src/components/modules/ScrollInteraction1.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-// interface TextBlockProps extends HeadingProps {
-//   fullWidth?: boolean;
-//   hidden?: boolean;
-// }
+const determineOpacity = (top) => (100 - (Math.abs(top) * 0.6)) * 0.01
 
-
-
-const ScrollInteraction1 = ({offsetY}) => {
+const ScrollInteraction1 = ({steps, offsetY}) => {  
 
   const [isTitleAnimToggled, setIsTitleAnimToggled] = useState(false)
   const [containerOffset, setContainerOffset] = useState({top: 0, bottom: 0})
   const [isTitleSticky, setIsTitleSticky] = useState(false)
+  const [rightTextOpacities, setRightTextOpacities] = useState(steps.map(((i, n) => n === 0 || n===1 ? 1 : 0)))
+  const [currentStep, setCurrentStep] = useState(0)
 
   const containerRef = useRef()
+  const rightTextRefs = useRef([])
   
   useEffect(() => {
     const { top, bottom } = containerRef.current.getBoundingClientRect()
@@ -21,35 +19,52 @@ const ScrollInteraction1 = ({offsetY}) => {
   }, [offsetY])
 
   useEffect(() => {
-    console.log(containerOffset.top, containerOffset.bottom);
     if(containerOffset.top <= 0){
       setIsTitleSticky(true)
-    }else{
+    } else {
       setIsTitleSticky(false)
     }
   }, [containerOffset])
 
+  const handleScroll = () => {        
+    const refsOpacity = rightTextRefs.current.map(i => determineOpacity(i.getBoundingClientRect().top))
+    setRightTextOpacities(refsOpacity)
+    setCurrentStep(refsOpacity.findIndex(i => i > 0.5))
+  }
+
+  useEffect(() => {
+    setIsTitleAnimToggled(currentStep === 3)
+  }, [rightTextOpacities])
+
   return(
     <section
-      className={`flex justify-center items-center bg-gray-200 h-screen ${isTitleSticky ? 'sticky top-0' : 'static'}`}
+      className={`flex justify-center items-center h-screen ${isTitleSticky ? 'sticky top-0' : 'static'}`}
       ref={containerRef}
     >
-      <div className="flex-grow">
+      <div className="flex flex-1 h-full pt-[312px] pr-8">
         <AnimatedTitle
           toggled={isTitleAnimToggled}
           isSticky={isTitleSticky}
         />
       </div>
-      <div className={`h-full flex-grow ${isTitleSticky ? 'overflow-scroll' : 'overflow-clip'} snap-y snap-mandatory`}>
-        <div className="h-screen flex items-center snap-start">
-          <p>Sample</p>
-        </div>
-        <div className="h-screen flex items-center snap-start">
-          <p>Sample</p>
-        </div>
-        <div className="h-screen flex items-center snap-start">
-          <p>Sample</p>
-        </div>
+      <div
+        className={`h-full flex-1 ${isTitleSticky ? 'overflow-scroll' : 'overflow-clip'} snap-y snap-mandatory`}
+        onScroll={handleScroll}
+      >
+        {
+          steps?.map((i, n) => n > 0 ? (
+            <div
+              key={n+''}
+              ref={el => rightTextRefs.current[n] = el}
+              className={`h-screen flex snap-start pt-[322px] max-w-[416px]`}
+              style={{
+                opacity: rightTextOpacities[n]
+              }}
+            >
+              <p className="text-4xl leading-snug">{i.right.text.replace(/\*/g, '')}</p>
+            </div>
+          ) : <div ref={el => rightTextRefs.current[n] = el}/>)
+        }
       </div>
     </section>
   )
@@ -72,9 +87,9 @@ const AnimatedTitle = ({toggled, isSticky}) => {
   }, [toggled])
 
   return (
-    <div className={`flex flex-col w-full`}>
+    <div className={`flex flex-col w-full h-full`}>
       <div className="flex justify-end children:ease-in-out children:transform children:transition-all children:duration-700">
-        <span className={`mr-1`}>Making</span>
+        <span className={`text-[58px] leading-[72px] mr-3`}>Making</span>
         <div
           className={`relative`}
           style={{
@@ -83,13 +98,13 @@ const AnimatedTitle = ({toggled, isSticky}) => {
             width: wordWidth,
           }}
         >
-          <span ref={theRef} className={`absolute transition-opacity duration-500 right-0 ${toggled ? 'opacity-0' : 'opacity-100 delay-300'}`}>the</span>
-          <span ref={itRef} className={`absolute transition-opacity duration-500 right-0 ${toggled ? 'opacity-100' : 'opacity-0'}`}>it</span>
+          <span ref={theRef} className={`text-[58px] leading-[72px] absolute transition-opacity duration-500 right-0 ${toggled ? 'opacity-0' : 'opacity-100 delay-300'}`}>the</span>
+          <span ref={itRef} className={`text-[58px] leading-[72px] absolute transition-opacity duration-500 right-0 ${toggled ? 'opacity-100' : 'opacity-0'}`}>it</span>
         </div>
       </div>
-      <div className="relative children:ease-in-out children:transform children:transition-all children:duration-500">
-        <span className={`absolute top-0 right-0  ${toggled ? 'opacity-0' : 'opacity-100'}`}>impossible,</span>
-        <span className={`delay-150 absolute top-0 right-0  ${!toggled ? 'translate-y-full' : 'translate-y-0'}`}>possible.</span>
+      <div className="relative children:ease-in-out children:transform children:transition-all children:duration-500 h-[144px]">
+        <span className={`text-[58px] leading-[72px] absolute top-0 right-0  ${toggled ? 'opacity-0' : 'opacity-100'}`}>impossible,</span>
+        <span className={`text-[58px] leading-[72px] delay-150 absolute top-0 right-0  ${!toggled ? 'translate-y-full' : 'translate-y-0'}`}>possible.</span>
       </div>
     </div>
   )
