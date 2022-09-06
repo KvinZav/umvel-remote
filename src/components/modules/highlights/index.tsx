9cf43a0e25fe3a844aade5196ba5635ffeeab2bc
Paco <francisco.garcia@umvel.com>
2022-09-06 17:07:15 -0500

    fix: change transition

--- src/components/modules/highlights/index.tsx
+++ src/components/modules/highlights/index.tsx
@@ -2,27 +2,27 @@ import useMediaQuery from "@hooks/useMediaQuery"
 import React,{useState} from "react"
 import HighlightsPhone from "./phone";
 import HighlightsTablet from "./tablet";
-import HighlightsDesktop from "./desktop";
-
-// temporal
-interface hola  { 
-    id: number;
-    title: string;
-    subtitle: string;
-    description: string;
-    imgUrl: string;
-}
+import HighlighsDesktop from "./desktop";
+import useSWR from "swr";
+import { environment } from "@environments/index";
+import { FETCHER } from "@fetcher/clients";
+import { BlockNameEnum } from "@enums/BlockName";
+import { Daum } from "@interfaces/home-data/home.interface";
 
 const CasesHighlights:React.FC = ():JSX.Element => {
-    const [project,setProject] = useState<hola>();
+    const [project,setProject] = useState<Daum>();
     const phone= useMediaQuery('(max-width: 640px)');
     const tablet= useMediaQuery('(max-width: 1024px) and (min-width: 640px)');
     const desktop= useMediaQuery('(min-width: 1024px)');
+    const { data: event } = useSWR(environment.HOME_URL)
+
+    if (!event) return null;
+    const {caseSelector} = FETCHER(event, BlockNameEnum.portfolio)
+    const projects = caseSelector.selector.case_of_studies.data;
 
     if(project===undefined){
         setProject(projects[0]);
     }
-
     const handleNext = () => {
         if(project.id<projects.length){
             setProject(projects[(project.id)])
@@ -40,54 +40,9 @@ const CasesHighlights:React.FC = ():JSX.Element => {
         }
     }
 
-    if(phone){return <HighlightsPhone projects={projects}/>}
-    if(tablet){return <HighlightsTablet project={project} handleNext={handleNext} handlePrevious={handlePrevious}/>}
-    if(desktop){return <HighlightsDesktop project={project} handleNext={handleNext} handlePrevious={handlePrevious} />} 
+    if(phone){return <HighlightsPhone projects={projects} title={caseSelector.title}/>}
+    if(tablet){return <HighlightsTablet project={project} handleNext={handleNext} handlePrevious={handlePrevious} title={caseSelector.title}/>}
+    if(desktop){return <HighlighsDesktop project={project} handleNext={handleNext} handlePrevious={handlePrevious} title={caseSelector.title}/>} 
 }
 
 export default CasesHighlights
-
-const projects = [
-    {
-        id:1,
-        title:"Viva aerobus1",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    },
-    {
-        id:2,
-        title:"Viva aerobus2",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    },
-    {
-        id:3,
-        title:"Viva aerobus3",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    },
-    {   
-        id:4,
-        title:"Viva aerobus4",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    },
-    {
-        id:5,
-        title:"Viva aerobus5",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    },
-    {   
-        id:6,
-        title:"Viva aerobus6",
-        subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        imgUrl:"https://picsum.photos/500/1200",
-    }
-]
