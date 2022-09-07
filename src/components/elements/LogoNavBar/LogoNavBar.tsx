4c020c08338405748ee9090b2da14088acf49a29
Paco <francisco.garcia@umvel.com>
2022-09-07 18:42:12 -0500

    fix: adjust size for Logo

--- src/components/elements/LogoNavBar/LogoNavBar.tsx
+++ src/components/elements/LogoNavBar/LogoNavBar.tsx
@@ -1,9 +1,10 @@
 import type { LogoPorps } from "@type/components/LogoNavBar"
+import Image from 'next/image'
 
 const Logo: React.FC<LogoPorps> = ({ imgUrl, alt, caption, url }) => {
     return (
         <a href={url ? url : '#'} className="flex items-center">
-            <img src={imgUrl} className="mr-3 h-6 sm:h-9" alt={alt} />
+            <Image src={imgUrl} width='100%' height='100%' alt={alt}/>
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{caption}</span>
         </a>
     )
