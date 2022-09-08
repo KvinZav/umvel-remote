3a5aae03597331a0ac8cc1f54557e0db8659d601
KevinZav <kevin.zavala@umvel.com>
2022-09-08 10:44:05 -0500

    fix: change elements sizes on responsive cases

--- src/components/modules/footer/FooterCta.tsx
+++ src/components/modules/footer/FooterCta.tsx
@@ -1,9 +1,9 @@
 import BasicButton from '@elements/button';
 import { CustomCard } from '@elements/card/card';
+import CustomImage from '@elements/image/CustomImage';
 import { BlockNameEnum } from '@enums/BlockName';
 import { environment } from '@environments/index';
 import { FETCHER } from '@fetcher/clients';
-import Image from 'next/image';
 import React from 'react';
 import useSWR from 'swr';
 
@@ -16,10 +16,10 @@ const FooterCta = () => {
 
   return(
     <div className="grid md:grid-cols-2 bg-primary-black">
-      <CustomCard customStyles="px-12 xl:px-32 flex flex-col items-center justify-center" borderless>
-        <div className="flex flex-col">
-          <h1 className="text-[26px] md:text-[27px] xl:text-[58px] leading-tight font-bold text-primary-white pb-6 xl:pb-8">{left.title}</h1>
-          <p className="text-base xl:text-lg leading-tight text-primary-white pb-6 xl:pb-8">{left.subtitle}</p>
+      <CustomCard customStyles="flex flex-col items-center justify-center" borderless>
+        <div className="flex flex-col w-[280px] lg:w-[420px]">
+          <h1 className="text-[26px] md:text-[27px] lg:text-[58px] leading-tight font-bold text-primary-white pb-6 xl:pb-8">{left.title}</h1>
+          <p className="text-base lg:text-lg leading-tight text-primary-white pb-6 xl:pb-8">{left.subtitle}</p>
           <BasicButton theme="dark">Lets Talk!</BasicButton>
         </div>
       </CustomCard>
@@ -27,7 +27,12 @@ const FooterCta = () => {
         {
           [...new Array(144)].map((_, n) => <div key={n+''} className="border-[0.5px] border-[#333333]"/>)
         }
-        <img className="absolute w-full p-28" src={'/assets/images/umvelLogo.svg'}/>
+        <div className="absolute w-full p-[24vw] md:p-[12vw]">
+          <CustomImage 
+            src={'/assets/images/umvelLogo.svg'}
+            alt={'logo-umvel'}
+            className="w-full"/>
+        </div>
       </CustomCard>
     </div>
   )
