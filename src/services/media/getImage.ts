c74cfa04a121bcdf09e3c8ae80f45ae243f0fe3f
Paco <francisco.garcia@umvel.com>
2022-09-08 11:36:31 -0500

    fix: change components for images on navbar

--- src/services/media/getImage.ts
+++ src/services/media/getImage.ts
@@ -1,7 +1,6 @@
 import { API_URL } from "@environments/index";
 
 export const getStrapiMedia = (url: string) => {
-    //const imageUrl = url.startsWith("/") ? `${API_URL}${url}` : url;
-    //return imageUrl;
-    return url
+    const imageUrl = url.startsWith("/") ? `${API_URL}${url}` : url;
+    return imageUrl;
 }
