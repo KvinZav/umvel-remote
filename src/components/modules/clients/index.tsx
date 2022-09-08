d7bf0eef870b992b27ad3e542ee0fca6717ce8e4
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-09-08 17:33:41 -0500

    fix: responsive margins clients

--- src/components/modules/clients/index.tsx
+++ src/components/modules/clients/index.tsx
@@ -21,23 +21,21 @@ export const Clients = () => {
 
 
     return (
-        <>
-            <div className="grid place-content-center min-h-[912px]">
-                <div className="grid gap-y-12 md:gap-y-4 lg:gap-y-4 mb-20">
-                    <ClientsTitle>{title}</ClientsTitle>
-                    <ClientsSubTitle>{subtitle}</ClientsSubTitle>
-                </div>
-                <div className="grid grid-cols-2 gap-10 md:gap-y-7 md:gap-x-6  lg:grid-cols-3 lg:gap-x-60 lg:gap-y-10">
-                    {clients.data.map(({ id, attributes }) => (
-                        <div key={id}
-                            data-info={attributes.name}
-                            onMouseEnter={handleMouseOver}
-                        >
-                            <ClientsText>{attributes.name}</ClientsText>
-                        </div>
-                    ))}
-                </div>
+        <div className="grid place-content-center py-[104px] md:py-[219px] lg:py-80 px-[72px] md:px-36">
+            <div className="grid gap-y-4 mb-20">
+                <ClientsTitle>{title}</ClientsTitle>
+                <ClientsSubTitle>{subtitle}</ClientsSubTitle>
             </div>
-        </>
+            <div className="grid grid-cols-2 gap-10 gap-y-8 md:gap-x-6 lg:grid-cols-3 lg:gap-x-60 lg:gap-y-10">
+                {clients.data.map(({ id, attributes }) => (
+                    <div key={id}
+                        data-info={attributes.name}
+                        onMouseEnter={handleMouseOver}
+                    >
+                        <ClientsText>{attributes.name}</ClientsText>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
