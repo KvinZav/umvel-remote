1854f88d23f73f341bfeb7b2a1869116f2540459
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-09-19 09:54:48 -0500

    fix: solve conflicts

--- src/components/elements/WorkCase/WorkCase.tsx
+++ src/components/elements/WorkCase/WorkCase.tsx
@@ -1,31 +1,24 @@
 import React from "react";
 import { WorkCaseProps } from "@interfaces/components/WorkCase";
 import Image from '@elements/image-component/index';
+import BasicButton from "@elements/button";
+import Link from "next/link";
 
 const WorkCase: React.FC<WorkCaseProps> = ({ project, inverted, alignImage }): JSX.Element => {
     return (
-        <>
-           {project.quote && <div className="hidden sm:flex w-full h-[30vw] flex-col justify-center items-center text-lg">
-                <p>{project.quote}</p>
-            </div>}
-            <article className={inverted ? `w-full flex flex-col sm:flex-row` : `w-full flex flex-col sm:flex-row-reverse`}>
-                <div className="w-full sm:w-1/2 sm:aspect-square bg-primary-white flex justify-center p-4 flex-col sm:p-16 lg:p-32">
-                    <p className="text-3xl font-bold mb-4 mt-[6rem] sm:mt-0">{project.title}</p>
-                    <p className="text-xl font-bold mb-4">{project.hoverClientName}</p>
-                    <p className="mb-4">{project.hoverDescription}</p>
-                    <div className="border rounded-full w-min px-4 py-3">
-                        <a href={project.callToAction}>
-                            <p className="inline-block whitespace-nowrap">View Cases</p>
-                        </a>
-                    </div>
-                </div>
-                <div className={`w-full sm:w-1/2 aspect-square bg-${project.backgroundColor} flex justify-center items-center overflow-hidden`}>
-                    <div className={alignImage === 'right' ? "translate-x-[20%]" : alignImage === 'left' ? "-translate-x-[20%]" : ""}>
-                        <Image url={project.image.data.attributes.url} alternativeText="project" layout="intrinsic" height={'673'} width={'977'} />
-                    </div>
-                </div>
-            </article>
-        </>
+        <article className={inverted?`w-full bg-prisma-navy flex flex-col sm:flex-row`:`w-full flex flex-col sm:flex-row-reverse`}>
+            <div className="w-full sm:w-1/2 sm:aspect-square bg-primary-white flex justify-center p-4 flex-col sm:p-16 lg:p-32">
+                <p className="text-3xl font-bold mb-4 mt-[6rem] sm:mt-0">Viva Aerobus</p>
+                <p className="text-xl font-bold mb-4">Mobile App</p>
+                <p className="mb-4">You love flying. Now you’ll love getting tickets too.</p>
+                <Link href="#">
+                    <BasicButton>View Cases</BasicButton>
+                </Link>
+            </div>
+            <div className="w-full sm:w-1/2 aspect-square bg-prisma-lemon flex justify-center items-center">
+                <Image url="https://raw.githubusercontent.com/franciscojagarcia/assets-umvel/main/Property%201%3DDefault.svg" alternativeText="project" layout="intrinsic" height={'673'} width={'977'} />
+            </div>
+        </article>
     )
 }
 
--- src/components/modules/workCases/index.tsx
+++ src/components/modules/workCases/index.tsx
@@ -1,20 +1,60 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import WorkCase from "@elements/WorkCase/WorkCase";
 import useSWR from "swr";
 import { environment } from "@environments/index";
 
 const WorkCases: React.FC = (): JSX.Element => {
-    const { data: event } = useSWR(environment.OUR_WORK_URL)
-    if (!event) return null;
-    const cases = event.data.attributes.body[0].cases;
+
+    const workCaseRefs = useRef<HTMLDivElement[]>([])
+    const mainContainerRef = useRef<HTMLDivElement>(null)
+    // const [sectionPages, setSectionPages] = useState<Array<any>>(sections)
+    const [currentPage, setCurrentPage] = useState<number>(0)
+
+    const handleScroll = event => setCurrentPage(Math.round(event.currentTarget.scrollTop / event.currentTarget.offsetHeight))
+
     return (
-        <>
+        <main
+            className="relative h-screen w-screen overflow-scroll snap-proximity snap-y"
+            ref={mainContainerRef}
+            onScroll={handleScroll}
+        >
             {
-                cases.map((project, index) => 
-                (<WorkCase project={project} inverted={index % 2 === 0} key={project.title+index} alignImage={'left'} />)
-                )
+                [...new Array(6)].map((_, index) => {
+                    return (
+                        <div
+                            ref={el => workCaseRefs.current[index] = el}
+                            key={'project-' + index}
+                            id={'project-' + index}
+                            className={`z-0 w-full flex flex-col justify-center items-center snap-center`}
+                        >
+                            <WorkCase inverted={index % 2 === 0} />
+                        </div>
+                    )
+                })
             }
-        </>
+            <div className="w-full h-[30vw] flex flex-col justify-center items-center">
+                <p>We deliver what</p>
+                <p>we promise.</p>
+            </div>
+
+            <div className="h-full fixed top-0 right-0 z-50 flex flex-col justify-center space-y-10 px-[34px]">
+                {
+                    [...new Array(6)].map((_, index) => (
+                        <div key={'indicator-' + index} className="group relative">
+                            <div className={`w-3 h-3 rounded-full ${index === currentPage ? 'bg-secondary-50' : 'bg-secondary-20'}`} />
+                            <button
+                                //Negative right offset 19px = 34px (from indicator container margin) - 16px (from label padding) + 1px (from border width)
+                                className={`absolute flex -top-[160%] -right-[19px] border border-secondary-10 rounded-full py-3 px-4 space-x-4 justify-center items-center transition duration-500 opacity-0 group-hover:opacity-100`}
+                                onClick={() => workCaseRefs.current[index].scrollIntoView({ behavior: 'smooth' })}
+                            >
+                                <span className="whitespace-nowrap origin-right transition-[max-width] duration-1000 max-w-0 group-hover:max-w-[85px] overflow-hidden">{index}</span>
+                                <div className={`w-4 h-4 bg-prisma-blue`} />
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
+        </main>
     )
 }
 
