1b1fa73003752f639c4592d6c82b5888634cf6a6
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-09-19 21:45:51 -0500

    fix: correct rollover animation

--- src/components/modules/hero.tsx
+++ src/components/modules/hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
           <Card
             styles={{
               textStyles: { height: "subtitle", align: "start" },
-              textPositionHorizontal: "end",
+              textPositionHorizontal: "start",
               textPositionVertical: "end",
               bg: 'white'
             }}
