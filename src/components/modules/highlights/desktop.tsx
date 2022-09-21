94f8c69b5d89f89e78bfc3672fc8c57b7979276a
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-09-21 13:50:52 -0500

    fix: prism button

--- src/components/modules/highlights/desktop.tsx
+++ src/components/modules/highlights/desktop.tsx
@@ -5,6 +5,7 @@ import { HighlightsDesktopPorps } from "@type/modules/highlights"
 import React, { useEffect, useRef, useState } from "react"
 import useSWR from "swr"
 import Image from "@elements/image-component";
+import CustomImage from "@elements/image-component/CustomImage"
 
 const HighlightsDesktop:React.FC<HighlightsDesktopPorps> = ({project,handleNext,handlePrevious, title}):JSX.Element => {
     const isBrowser = typeof window !== "undefined"
@@ -41,15 +42,25 @@ const HighlightsDesktop:React.FC<HighlightsDesktopPorps> = ({project,handleNext,
                         </div>
                     </div>
                     <div className="col-start-1 col-span-1 row-start-3 row-span-1 bg-primary-white w-full aspect-square text-primary-black" style={{ transform: `translateY(${positionInfo}px)`, transformOrigin: 'top left' }}>
-                        <div className="h-full flex flex-col justify-center ">
+                        <div className="h-full flex flex-col justify-center bgred">
                             <div>
                                 <h3 className="text-xl font-bold mb-4 mx-8 mt-8">{title}</h3>
                                 <p className="font-sm mx-8 mb-4 text-base">Explore more cases:</p>
                             </div>
-                            <div className="h-1/2 flex flex-row justify-center items-center">
-                                <button className="mr-4" onClick={() => handlePrevious()}>{'<'}</button>
+                            <div className="h-1/2 flex flex-row justify-between items-center px-11">
+                                <button className="mr-4" onClick={() => handlePrevious()}>
+                                    <CustomImage
+                                        src="/assets/icons/chevronLeft.svg"
+                                        alt="icon-chevron-left"
+                                    />
+                                </button>
                                 <PrismButton>Show more</PrismButton>
-                                <button className="ml-4" onClick={() => handleNext()}>{'>'}</button>
+                                <button className="mr-4" onClick={() => handlePrevious()}>
+                                    <CustomImage
+                                        src="/assets/icons/chevronRight.svg"
+                                        alt="icon-chevron-right"
+                                    />
+                                </button>
                             </div>
                         </div>
                     </div>
