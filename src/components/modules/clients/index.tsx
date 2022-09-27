8c9e4c02f65e72cc8578176a0a3c9dde9f09a876
Paco <francisco.garcia@umvel.com>
2022-09-27 17:15:00 -0500

    fix: add hover on clients

--- src/components/modules/clients/index.tsx
+++ src/components/modules/clients/index.tsx
@@ -4,8 +4,6 @@ import { FETCHER } from '@fetcher/clients';
 import { BlockNameEnum } from "@enums/BlockName";
 
 import useSWR from "swr";
-import { animated, AnimatedProps, useSpringRef, useTransition, useSpring } from "react-spring";
-import { CSSProperties, useCallback, useEffect, useState } from "react";
 
 export const Clients = () => {    
 
@@ -15,17 +13,25 @@ export const Clients = () => {
 
 
     return (
-        <div className="grid place-content-center py-[104px] md:py-[219px] lg:py-80 px-[72px] md:px-36">
+        <div className="grid place-content-center py-[104px] md:py-[219px] lg:pt-80 px-[72px] md:px-36">
             <div className="grid gap-y-4 mb-20">
                 <ClientsTitle>{title}</ClientsTitle>
                 <ClientsSubTitle>{subtitle}</ClientsSubTitle>
             </div>
             <div className="grid grid-cols-2 gap-10 gap-y-8 md:gap-x-6 lg:grid-cols-3 lg:gap-x-60 lg:gap-y-10">
-                {clients.data.map(({ id, attributes }) => (
+                {clients.data.map(({ id, attributes },index) => (
                     <div key={id}
                         data-info={attributes.name}
+                        className="group lg:hover:text-primary-white"
                     >
-                        <ClientsText>{attributes.name}</ClientsText>
+                            <div 
+                                className={(index+1)%3!==0
+                                    ?"hidden lg:group-hover:flex bg-cover absolute -z-10 w-96 h-96 -translate-y-80 -translate-x-8"
+                                    :"hidden lg:group-hover:flex bg-cover absolute -z-10 w-96 h-96 -translate-y-80 -translate-x-[60%]"
+                                }
+                                style={{backgroundImage: `url(${attributes.image.data.attributes.url})`}}
+                            />
+                            <ClientsText>{attributes.name}</ClientsText>
                     </div>
                 ))}
             </div>
