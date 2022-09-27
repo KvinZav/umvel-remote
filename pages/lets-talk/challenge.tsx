9c86d9214ee40875eafbffba9809910f6c9350a0
ljavierma <luisjavier.montielarenas@gmail.com>
2022-09-26 22:54:02 -0500

    feat: lets talk section implementation

---
+++ pages/lets-talk/challenge.tsx
@@ -0,0 +1,43 @@
+import BasicButton from "@elements/button";
+
+const Challenge = () => {
+  return (
+    <div className="aspect-square lg:p-10 md:p-32 px-[72px] pt-7 pb-[132px] md:border-solid md:border-[#e6e6e6] md:border">
+      <p className="md:text-[28px] text-2xl font-bold leading-8">We’re up for the challenge</p>
+      <p className="text-base mt-2">
+        Provide your details and we’ll get in touch as soon as humanly possible.
+      </p>
+      <form className="lg:mt-10 md:mt-12 mt-10">
+        <input
+          type="text"
+          className="w-full pl-0 pr-0 pb-2 h-[32px] border-solid border-b-[#e6e6e6] border-b placeholder:text-[#000] focus:border-b-[#000]"
+          placeholder="Name"
+        />
+        <div className="md:flex mt-10">
+          <input
+            type="text"
+            className="md:w-1/2 w-full md:mr-8 mr-0 pl-0 pr-0 pb-2 h-[32px] border-solid border-b-[#e6e6e6] border-b placeholder:text-[#000] focus:border-b-[#000]"
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            className="md:w-1/2 w-full md:mt-0 mt-10 pl-0 pr-0 pb-2 h-[32px] border-solid border-b-[#e6e6e6] border-b placeholder:text-[#000] focus:border-b-[#000]"
+            placeholder="Phone (optional)"
+          />
+        </div>
+        <input
+          type="text"
+          className="w-full mt-10 pl-0 pr-0 pb-2 h-[32px] border-solid border-b-[#e6e6e6] border-b placeholder:text-[#000] focus:border-b-[#000]"
+          placeholder="Tell us about your project, question or comment. (optional)"
+        />
+        <div className="lg:mt-10 md:mt-12 mt-10 flex justify-end">
+          <BasicButton theme="light">
+            <p>Send!</p>
+          </BasicButton>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Challenge;
---
+++ pages/lets-talk/find-us.tsx
@@ -0,0 +1,89 @@
+import CustomImage from "@elements/image-component/CustomImage";
+
+const FindUs = () => {
+  return (
+    <>
+      <div className="aspect-square lg:p-10 bg-primary-black text-primary-white lg:flex hidden flex-col justify-between">
+        <div>
+          <p className="text-sm leading-4">Or find us here</p>
+          <p className="text-[28px] leading-8 mt-2 font-bold">
+            Feel free to reach out directly, either in person, per phone or email.
+          </p>
+          <div className="flex mt-12">
+            <div className="w-1/2 mr-8">
+              <p className="text-sm leading-4">Pay us a visit</p>
+              <p className="text-lg leading-6 font-bold mt-2">
+                Avenida Amsterdam 173, 06100, Condesa,
+                <br />
+                CDMX, Mexico
+              </p>
+            </div>
+            <div className="w-1/2">
+              <p className="text-sm leading-4">Call us</p>
+              <p className="text-lg leading-6 font-bold mt-2">(+52) 55 6819 7071</p>
+              <p className="text-sm leading-4 mt-12">Email us</p>
+              <p className="text-lg leading-6 font-bold mt-2">hello@umvel.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="self-end">
+          <CustomImage
+            src={"/assets/images/umvelLogo.svg"}
+            alt={"logo-umvel"}
+            className="w-[64px]"
+          />
+        </div>
+      </div>
+
+      <div className="bg-primary-black md:grid grid-cols-3 text-primary-white lg:hidden hidden">
+        <div className="aspect-square border-solid border-[#333] border  p-8">
+          <p className="text-sm leading-4">Or find us here</p>
+          <p className="text-xl leading-6 mt-2 font-bold">
+            Feel free to reach out directly, either in person, per phone or email.
+          </p>
+        </div>
+        <div className="aspect-square border-solid border-[#333] border p-8">
+          <p className="text-sm leading-4">Pay us a visit</p>
+          <p className="text-xl leading-6 mt-2 font-bold">
+            Avenida Amsterdam 173, 06100, Condesa,
+            <br />
+            CDMX, Mexico
+          </p>
+        </div>
+        <div className="aspect-square border-solid border-[#333] border p-8">
+          <p className="text-sm leading-4">Email us</p>
+          <p className="text-xl leading-6 mt-2 font-bold">hello@umvel.com</p>
+          <p className="text-sm leading-4 mt-8">Call us</p>
+          <p className="text-xl leading-6 mt-2 font-bold">(+52) 55 6819 7071</p>
+        </div>
+      </div>
+
+      <div className="bg-primary-black grid grid-cols-2 text-primary-white lg:hidden md:hidden">
+        <div className="aspect-square border-solid border-[#333] border p-6">
+          <p className="text-sm leading-4">Or find us here</p>
+          <p className="text-base mt-2 font-bold">
+            Feel free to reach out directly, either in person, per phone or email.
+          </p>
+        </div>
+        <div className="aspect-square border-solid border-[#333] border p-6">
+          <p className="text-sm leading-4">Pay us a visit</p>
+          <p className="text-base mt-2 font-bold">
+            Avenida Amsterdam 173, 06100, Condesa,
+            <br />
+            CDMX, Mexico
+          </p>
+        </div>
+        <div className="aspect-square border-solid border-[#333] border p-6">
+          <p className="text-sm leading-4">Email us</p>
+          <p className="text-base mt-2 font-bold">hello@umvel.com</p>
+        </div>
+        <div className="aspect-square border-solid border-[#333] border p-6">
+          <p className="text-sm leading-4">Call us</p>
+          <p className="text-base mt-2 font-bold">(+52) 55 6819 7071</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FindUs;
