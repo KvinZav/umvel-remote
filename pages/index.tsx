d48435316923ad6a9e45bb312ac5068848f36dc7
Paco <francisco.garcia@umvel.com>
2022-09-26 19:03:12 -0500

    fix: add global footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,8 +55,6 @@ export default function Home() {
         data={data.data.attributes.body
           .find((item) => item.__component === BlockNameEnum.team)}
       />
-      <FooterMenu data={data.data.attributes.body
-          .find((item) => item.__component === BlockNameEnum.menu)}/>
     </div>
   );
 }
