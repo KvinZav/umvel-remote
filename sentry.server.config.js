28d06dfe00aad280caa395c19a1af1debe20bb53
paco <francisco.garcia@umvel.com>
2022-09-29 17:24:37 -0500

    fix: resolve sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: SENTRY_DSN,
     tracesSampleRate: 1.0,
   });
 
