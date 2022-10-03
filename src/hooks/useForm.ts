146a86908372ab8f5838cd8609af397260694c07
KevinZav <kevin.zavala@umvel.com>
2022-10-03 13:29:54 -0500

    feat: implement multiple validations on useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,31 +1,70 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const formatFields = (fields: {[key: string]: [string, any?]}) => {
+const formatFields = (fields: { [key: string]: [string, (Function | Function[])?] }) => {
   const result = {};
   Object.keys(fields).forEach((fieldName) => {
+    const defaultValidation = () => true;
+    const { error, errors } = verifyValidations(
+      fields[fieldName][0],
+      fields[fieldName][1] || defaultValidation
+    );
     result[fieldName] = {
       value: fields[fieldName][0],
-      validation: fields[fieldName][1] || function() {return true},
-      error: false,
+      validations: fields[fieldName][1] || defaultValidation,
+      error,
+      errors,
     };
   });
   return result;
-}
+};
 
-const verifyValidation = ((value: string, validation: Function) => {
-  return !validation(value);
-});
+const verifyValidations = (value: string, validations: Function | Function[]) => {
+  if (typeof validations === 'function') {
+    const verifyValidation = !validations(value);
+    if (verifyValidation) {
+      return {
+        error: true,
+        errors: [
+          {
+            error: true,
+            name: validations.name,
+          },
+        ],
+      };
+    } else {
+      return {
+        error: false,
+        errors: [],
+      };
+    }
+  } else {
+    const verifyValidations = validations.map((validation) => {
+      return {
+        error: !validation(value),
+        name: validation.name,
+      };
+    });
 
-export const useForm = (fields: {[key: string]: [string, Function?]}) => {
+    const error = verifyValidations.some((validation) => validation.error);
+    return {
+      error,
+      errors: verifyValidations.filter((validation) => validation.error),
+    };
+  }
+};
+
+export const useForm = (fields: { [key: string]: [string, (Function | Function[])?] }) => {
   const [formState, setFormState] = useState<any>(() => formatFields(fields));
-  
+
   const onChangeInput = ({ target }) => {
     const { name, value } = target;
     const currentField = formState[name];
+    const { error, errors } = verifyValidations(value, currentField.validations);
     const newValue = {
       ...currentField,
       value,
-      error: verifyValidation(value, currentField.validation)
+      error,
+      errors,
     };
     setFormState({
       ...formState,
