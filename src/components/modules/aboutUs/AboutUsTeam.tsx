0b9d51f141fde6e1bff7c8f8900fa75e805e5959
KevinZav <kevin.zavala@umvel.com>
2022-10-04 13:49:19 -0500

    fix: change padding to AboutUsTeam section

--- src/components/modules/aboutUs/AboutUsTeam.tsx
+++ src/components/modules/aboutUs/AboutUsTeam.tsx
@@ -26,13 +26,13 @@ export const AboutUsTeam = ({ data: { title, subtitle, teamMembers } }: { data: 
         <p className="text-base md:text-[28px]">{subtitle}</p>
       </div>
       <div
-        className="w-full grid grid-rows-2 md:flex md:flex-wrap md:justify-center gap-y-8 overflow-x-auto snap-x mb-[104px] md:mb-[200px] lg:mb-[320px]"
+        className="w-full grid grid-rows-2 md:flex md:flex-wrap md:justify-center gap-y-8 overflow-x-auto snap-x mb-[104px] md:mb-[200px] lg:mb-[320px] px-[10%]"
         style={customStyle}
       >
         {teamMembers.map((member, idx) => (
           <div key={`team-member-${idx}`} className="flex flex-col items-center md:w-1/3 lg:w-1/5">
             <Avatar
-              className="border border-[#D9D9D9] rounded-full"
+              className="border border-[#D9D9D9] rounded-full max-w-[90%]"
               photo={member.photo?.data.attributes.url || genericPhoto}
             />
             <div className="text-center">
