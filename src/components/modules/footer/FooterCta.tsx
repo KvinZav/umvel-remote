789e0ab6305e6046857087f520f7802e59aeffbe
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-10-05 17:30:26 -0500

    fix: add footer cta link

--- src/components/modules/footer/FooterCta.tsx
+++ src/components/modules/footer/FooterCta.tsx
@@ -2,6 +2,7 @@ import BasicButton from '@elements/button';
 import { BlockNameEnum } from '@enums/BlockName';
 import { environment } from '@environments/index';
 import { FETCHER } from '@fetcher/clients';
+import Link from 'next/link';
 import React from 'react';
 import useSWR from 'swr';
 import UmvelCard from './UmvelCard';
@@ -20,7 +21,11 @@ export const FooterCta = React.memo(({ theme = 'dark' }: { theme?: 'dark' | 'lig
         <div className='p-12 md:p-[8%_4vw] lg:p-[10%_5vw] xl:p-[182px]'>
           <h1 className={`text-m4 lg:text-b3 font-bold pb-6 xl:pb-8`}>{left.title}</h1>
           <p className="text-s1 pb-6 xl:pb-8">{left.subtitle}</p>
-          <BasicButton theme={theme}>{'Let\'s Talk!'}</BasicButton>
+          <Link href="/lets-talk">
+            <a>
+              <BasicButton theme={theme}>{'Let\'s Talk!'}</BasicButton>
+            </a>
+          </Link>
         </div>
     </UmvelCard>
   );
