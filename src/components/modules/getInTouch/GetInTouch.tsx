4ae38c6bb812efb0c33204e688089561a6e2d502
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-10-05 16:45:41 -0500

    fix: correct lowercase

--- src/components/modules/getInTouch/GetInTouch.tsx
+++ src/components/modules/getInTouch/GetInTouch.tsx
@@ -63,7 +63,7 @@ export const GetInTouch = () => {
   return (
     <section className="w-full mt-[104px] md:mt-0 mb-[96px] md:mb-[224px] px-16 lg:grid grid-cols-[200px_420px] xl:grid-cols-[150px_150px_600px] gap-x-8 lg:gap-y-4 grid-rows-2 lg:px-[20%] xl:px-0 items-center place-content-center">
       <div className="px-[72px] md:px-0 col-span-1 xl:col-span-2 row-span-2 mb-">
-        <h3 className="font-bold text-b3 text-center ">Get in Touch!</h3>
+        <h3 className="font-bold text-b3 text-center ">Get in touch!</h3>
       </div>
       <div
         className="text-center mb-8 lg:mb-0 lg:text-left col-span-1 row-span-1 flex justify-center"
