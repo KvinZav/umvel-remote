8721f050e42c76621c743b9d4ef4b961e494ecec
Paco <francisco.garcia@umvel.com>
2022-10-06 13:26:28 -0500

    fix: align center buttons

--- src/components/modules/whatWeOffer/modal-offers.tsx
+++ src/components/modules/whatWeOffer/modal-offers.tsx
@@ -35,7 +35,7 @@ export const ModalOffers = ({
             />
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center md:h-full">
           <div className="w-full md:w-3/5">
             <p className="text-b3 font-bold mb-6">What we offer</p>
             <div className="grid grid-cols-2 gap-4 mb-12">
