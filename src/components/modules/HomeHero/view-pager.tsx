7ebe4100ec6458f6a46e10b00dd7658cfa6f52ec
KevinZav <kevin.zavala@umvel.com>
2022-10-12 15:28:53 -0500

    fix: add container styles property on card component

--- src/components/modules/HomeHero/view-pager.tsx
+++ src/components/modules/HomeHero/view-pager.tsx
@@ -92,6 +92,7 @@ const ViewPager = ({cases}) => {
             bg: 'primary-white',
           }}
           text='We'
+          containerStyles={{padding: `${isMobile ? '8px' : '16px'}`}}
         />
       </div>
       <div className="row-span-1 col-span-1 relative overflow-hidden border border-secondary-10">
@@ -106,6 +107,7 @@ const ViewPager = ({cases}) => {
               textPositionVertical: 'end',
             }}
             text={cases[bottomCaseIndex]?.Title}
+            containerStyles={{padding: `${isMobile ? '8px' : '16px'}`}}
           />
         </div>
         {/* top */}
