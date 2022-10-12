f294ebf7cd2f27f5e596ec9dd8749004addc4ef2
KevinZav <kevin.zavala@umvel.com>
2022-10-12 15:31:45 -0500

    fix: solve typography issues

--- src/components/elements/WorkCase/WorkCase.tsx
+++ src/components/elements/WorkCase/WorkCase.tsx
@@ -20,9 +20,9 @@ const WorkCase: React.FC<WorkCaseProps> = ({
       }
     >
       <div className="md:w-1/2 aspect-square bg-primary-white flex md:justify-center py-6 px-12 flex-col md:px-16 md:py-16 lg:px-32 lg:py-32 xl:px-[180px] xl:py-[180px]">
-        <p className="text-m2 font-bold mb-2 xl:mb-4 md:mt-0">{project.title}</p>
-        <p className="text-s1 font-bold mb-2 xl:mb-4">{project.hoverClientName}</p>
-        <p className="text-s2 mb-4 md:mb-6 xl:mb-9">{project.hoverDescription}</p>
+        <p className="text-m2 lg:text-b4 font-bold mb-2 xl:mb-4 md:mt-0">{project.title}</p>
+        <p className="text-s1 lg:text-m4 font-bold mb-2 xl:mb-4">{project.hoverClientName}</p>
+        <p className="text-s2 lg:text-m4 mb-4 md:mb-6 xl:mb-9">{project.hoverDescription}</p>
         <Link href={project.callToAction || `/cases/${project.id}`}>
           <a>
             <BasicButton>View Case</BasicButton>
@@ -48,7 +48,7 @@ const WorkCase: React.FC<WorkCaseProps> = ({
     </article>
     {project.quote && (
       <div className="hidden md:flex w-full py-40 lg:py-[200px] xl:py-60 flex-col justify-center items-center">
-        <p className="text-m1 font-bold max-w-md text-center">{project.quote}</p>
+        <p className="text-m1 font-bold max-w-md xl:max-w-[600px] text-center">{project.quote}</p>
       </div>
     )}
   </>
