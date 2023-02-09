54527ad2854a1a36de3a418c31ab66e13cc26353
elvis.martinez <elvis.martinez@umvel.com>
2023-02-09 10:48:29 -0600

    feat: added format to files

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,23 +6,24 @@ import { useRouter } from 'next/dist/client/router';
 
 export default function FourOhFour() {
 
-    const router = useRouter();
+  const router = useRouter();
   return (
     <>
-    <UmvelCard darkTheme={true} is404={true} text={'While you wait, you could check our social media.'}>
+      <UmvelCard darkTheme={true} is404={true} text={'While you wait, you could check our social media.'}>
         <div className="p-12 md:p-[min(8%,_48px)] lg:p-[min(9%,_128px)] md:h-[100%] md:flex md:flex-col md:justify-between">
           <h1 className="mb-6 font-bold text-m3 md:text-b3 md:text-center lg:text-left">
             Seems like you <br /> got lost in the backrooms.
           </h1>
           <p className="mb-6 xl:mb-12 text-s1 md:text-m3 lg:text-s1 md:text-center lg:text-left">
-           Please, reaload the page or try to come <br/> back a few minutes later. 
+            Please, reaload the page or try to come <br /> back a few minutes later.
           </p>
           <div className='flex flex-col md:flex-row md:justify-center lg:justify-start'>
             <div className='flex items-center'>
               <button
-                className={`border rounded-full p-2 md:left-8 lg:left-[134px] text-[18px] border-primary-white text-primary-white lg:hover:text-primary-black lg:hover:bg-primary-white`}>
+                className={`border rounded-full p-2 md:left-8 lg:left-[134px] text-[18px] 
+                border-primary-white text-primary-white lg:hover:text-primary-black lg:hover:bg-primary-white`}>
                 <div className="w-[18px] h-[18px] flex justify-center text-[18px] leading-none">
-                  <ArrowBackRounded fontSize="inherit" onClick={() => router.back()}/>
+                  <ArrowBackRounded fontSize="inherit" onClick={() => router.back()} />
                 </div>
               </button>
               <p className='text-s6 ml-[10px]'>Return to previous page</p>
@@ -31,9 +32,9 @@ export default function FourOhFour() {
               <button
                 className={`border rounded-full p-2 md:left-8 lg:left-[134px] text-[18px] border-primary-white text-primary-white lg:hover:text-primary-black lg:hover:bg-primary-white`}>
                 <div className="w-[18px] h-[18px] flex justify-center text-[18px] leading-none">
-                <Link href={'/'}>
-                  <Home fontSize="inherit"/>
-                </Link>
+                  <Link href={'/'}>
+                    <Home fontSize="inherit" />
+                  </Link>
                 </div>
               </button>
               <p className='text-s6 ml-[10px]'>Home</p>
--- src/components/modules/footer/UmvelCard.tsx
+++ src/components/modules/footer/UmvelCard.tsx
@@ -7,7 +7,7 @@ import useSWR from 'swr';
 import { environment } from '@environments/index';
 import { get } from '@fetcher/get';
 
-const UmvelCard: FC<{ children?: React.ReactNode; darkTheme: boolean, text?:string, is404?: boolean }> = ({
+const UmvelCard: FC<{ children?: React.ReactNode; darkTheme: boolean, text?: string, is404?: boolean }> = ({
   children,
   darkTheme = false,
   text = '',
@@ -23,10 +23,9 @@ const UmvelCard: FC<{ children?: React.ReactNode; darkTheme: boolean, text?:stri
   return (
     <div className={`grid ${is404 ? 'lg:grid-cols-2' : 'md:grid-cols-2'}  ${darkTheme ? 'bg-primary-black' : ''}`}>
       <CustomCard
-        customStyles={`flex flex-col items-center  ${
-          !darkTheme ? 'border border-[#ccc]' : ''
-        } ${is404 ? 'md:justify-center':'lg:justify-center'}`
-      }
+        customStyles={`flex flex-col items-center  ${!darkTheme ? 'border border-[#ccc]' : ''
+          } ${is404 ? 'md:justify-center' : 'lg:justify-center'}`
+        }
         borderless
       >
         <div className={`flex flex-col ${darkTheme ? ' text-primary-white' : ''} ${is404 ? 'md:w-[75%] md:h-[70%]' : ''}`}>{children}</div>
@@ -35,34 +34,35 @@ const UmvelCard: FC<{ children?: React.ReactNode; darkTheme: boolean, text?:stri
         {[...new Array(144)].map((_, n) => (
           <div key={n + ''} className={`border ${darkTheme ? 'border-[#333]' : 'border-[#ccc]'}`} />
         ))}
-        <div className={`absolute w-full p-[15vw] md:p-[12vw] ${is404 ? 'pt-[50px] flex flex-col items-center':'' } `}>
-          <div className={`${is404 ? 'w-[130px] md:w-[220px] md:mt-[10%] lg:mt-0 lg:w-[300px]':''}`}>
-          <Image
-            url={`/assets/images/${darkTheme ? 'umvelLogo.svg' : 'umvelLogoDark.svg'}`}
-            alt={'logo-umvel'}
-            width="100%"
-            height="100%"
-            layout="responsive"
-          />
+        <div className={`absolute w-full p-[15vw] md:p-[12vw] ${is404 ? 'pt-[50px] flex flex-col items-center' : ''} `}>
+          <div className={`${is404 ? 'w-[130px] md:w-[220px] md:mt-[10%] lg:mt-0 lg:w-[300px]' : ''}`}>
+            <Image
+              url={`/assets/images/${darkTheme ? 'umvelLogo.svg' : 'umvelLogoDark.svg'}`}
+              alt={'logo-umvel'}
+              width="100%"
+              height="100%"
+              layout="responsive"
+            />
           </div>
-          {is404 && 
-          <div className='text-primary-white mt-[20px] md:mt-[30%] '>
-          <h1 className='text-center text-s2'>{text}</h1>
-          <div className="mt-0 md:mt-[30px] text-s4 md:text-s5 flex lg:justify-around justify-between md:flex-row  lg:border-0 py-8 lg:py-0 lg:min-w-[336px] xl:min-w-[521px]">
-            {socialNetworks.map((socialNetwork, index) => (
-              <a
-                className="leading-tight transform transition duration-150 hover:scale-105 hover:font-semibold"
-                key={`menu-item-footer-${index}`}
-                href={socialNetwork.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={`Visit Umvel's ${socialNetwork.name}`}
-              >
-                {socialNetwork.name}
-              </a>
-            ))}
-          </div>
-          </div>}
+          {is404 &&
+            <div className='text-primary-white mt-[20px] md:mt-[30%] '>
+              <h1 className='text-center text-s2'>{text}</h1>
+              <div className="mt-0 md:mt-[30px] text-s4 md:text-s5 flex lg:justify-around 
+          justify-between md:flex-row  lg:border-0 py-8 lg:py-0 lg:min-w-[336px] xl:min-w-[521px]">
+                {socialNetworks.map((socialNetwork, index) => (
+                  <a
+                    className="leading-tight transform transition duration-150 hover:scale-105 hover:font-semibold"
+                    key={`menu-item-footer-${index}`}
+                    href={socialNetwork.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={`Visit Umvel's ${socialNetwork.name}`}
+                  >
+                    {socialNetwork.name}
+                  </a>
+                ))}
+              </div>
+            </div>}
         </div>
       </CustomCard>
     </div>
