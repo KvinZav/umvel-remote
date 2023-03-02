0528787db2b6d9dda6c52079803f43fbb60310ff
KevinZav <kevin.zavala@umvel.com>
2023-03-02 00:08:19 -0600

    feat: add CardLogoAnimated component

---
+++ src/components/elements/card/CardLogoAnimated.tsx
@@ -0,0 +1,43 @@
+import CustomImage from '@elements/image-component/CustomImage';
+import { useIsSizeScreen } from '@hooks/useIsSizeScreen';
+import { useStartHomeAnimation } from '@hooks/useStartHomeAnimation';
+import { useEffect, useMemo } from 'react';
+
+export const CardLogoAnimated = () => {
+  const {
+    stylesLogo: styles,
+    isInitialized,
+    initializeAnimation,
+    classTransitions,
+  } = useStartHomeAnimation();
+  const { isMd, isLg, isXl, isSm } = useIsSizeScreen();
+  const widthImage = useMemo(() => {
+    if (isXl) return 70;
+    if (isLg || isMd) return 54;
+    if (isSm) return 30;
+  }, [isXl, isLg, isMd, isSm]);
+
+  useEffect(() => {
+    if (!isInitialized) {
+      initializeAnimation();
+    }
+  }, [initializeAnimation, isInitialized]);
+
+  return (
+    <div className={`w-full absolute grid grid-cols-${(isMd || isSm) ? 2 : 3}`}>
+      <div
+        className={
+          "flex items-center justify-end " + 
+          classTransitions.one
+        }
+        style={styles}
+      >
+        <CustomImage
+          src={'/assets/images/umvelLogoDark.svg'}
+          alt={'logo-home-init'}
+          style={{ width: `${widthImage}px` }}
+        />
+      </div>
+    </div>
+  );
+};
